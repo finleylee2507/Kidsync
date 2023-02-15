1c6c8fbe3e8f6b967bf1459c50c944a88ca73107
Che Hongle <chehongle123@163.com>
2023-02-14 22:01:24 -0600

    navbar prototype

--- src/components/Dependents/DependentsList.jsx
+++ src/components/Dependents/DependentsList.jsx
@@ -1,6 +1,11 @@
+import { Navbar } from "../Navbar";
+
 const DependentsList = () => {
   return (
-    <h1>This is where you will be able to see a list of your dependents.</h1>
+    <div>
+      <Navbar />
+      <h1>This is where you will be able to see a list of your dependents.</h1>
+    </div>
   );
 };
 
