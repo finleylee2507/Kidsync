a2cc989591f5adf5be6e17050dfc88421ca78fd9
elinarawatNU <elinarawat2024@u.northwestern.edu>
2023-02-16 22:05:14 -0600

    added Add Dependent button

--- src/components/Dependents/DependentsList.jsx
+++ src/components/Dependents/DependentsList.jsx
@@ -2,9 +2,37 @@ import data from "../../utilities/Dependents.json";
 import DependentCard from "./DependentCard";
 import styles from "./DependentsList.module.css";
 import { SimpleGrid, Container } from "@mantine/core";
+import useNavigate from "react-router-dom";
+
+const useStyles = createStyles(() => ({
+  button: {
+    position: 'relative',
+    transition: 'background-color 150ms ease',
+  },
+  label: {
+    position: 'relative',
+    zIndex: 1,
+  },
+}));
 
 const DependentsList = () => {
+  const { classes, theme } = useStyles();
+  const navigate = useNavigate();
+
+  const handleAddDependentClick = () => {
+    navigate("/create-dependents-profile")
+  };
+
   return (
+    <Button
+      fullWidth
+      className={classes.button}
+      // onClick={() => (loaded ? setLoaded(false) : !interval.active && interval.start())}
+      color={theme.primaryColor} > 
+      <div className={classes.label}>
+        Add Dependent
+      </div>
+    </Button>
     <Container py="xl">
       <SimpleGrid cols={2} breakpoints={[{ maxWidth: "md", cols: 1 }]}>
         {Object.entries(data["dependents"]).map(([id, dependent]) => (
