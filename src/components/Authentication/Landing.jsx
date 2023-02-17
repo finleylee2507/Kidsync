babc73d426a60eb6df6fc83630c2b7168c94386a
Finley Li <li.f@wustl.edu>
2023-02-17 00:16:04 -0600

    Fixed typos and invalid styles.

--- src/components/Authentication/Landing.jsx
+++ src/components/Authentication/Landing.jsx
@@ -1,93 +1,77 @@
-import { React } from "react";
-import {
-  addNewUser,
-  signInWithGoogle,
-  useAuthState,
-} from "../../utilities/firebase";
-import { Navigate, useNavigate } from "react-router-dom";
+import {React} from "react";
+import {addNewUser, signInWithGoogle, useAuthState,} from "../../utilities/firebase";
+import {Navigate, useNavigate} from "react-router-dom";
 import styles from "./Landing.module.css";
-import { FcGoogle } from "react-icons/fc";
-import { Grid } from "@mantine/core";
-import {
-  createStyles,
-  Image,
-  Container,
-  Title,
-  Button,
-  Group,
-  Text,
-  List,
-  ThemeIcon,
-} from "@mantine/core";
+import {FcGoogle} from "react-icons/fc";
+import {Image, Title} from "@mantine/core";
 
 const SignInButton = () => {
-  return (
-    <div className={styles.signInBtnContainer}>
-      <button
-        variant="light"
-        id={styles.signInButton}
-        className="btn btn-light"
-        size="lg"
-        aria-label="Sign in with google"
-        onClick={signInWithGoogle}
-      >
-        <FcGoogle className={styles.googleIcon} />
-        Sign in with Google
-      </button>
-    </div>
-  );
+    return (
+        <div className={styles.signInBtnContainer}>
+            <button
+                id={styles.signInButton}
+                className="btn btn-light"
+                size="lg"
+                aria-label="Sign in with google"
+                onClick={signInWithGoogle}
+            >
+                <FcGoogle className={styles.googleIcon}/>
+                Sign in with Google
+            </button>
+        </div>
+    );
 };
 
 const Landing = (allUsers) => {
-  const user = useAuthState();
-  const navigate = useNavigate();
+    const user = useAuthState();
+    const navigate = useNavigate();
 
-  if (user && allUsers && allUsers["allUsers"]) {
-    if (!allUsers["allUsers"][user.uid]) {
-      const newUser = {
-        displayName: user.displayName,
-        email: user.email,
-        photoURL: user.photoURL,
-      };
+    if (user && allUsers && allUsers["allUsers"]) {
+        if (!allUsers["allUsers"][user.uid]) {
+            const newUser = {
+                displayName: user.displayName,
+                email: user.email,
+                photoURL: user.photoURL,
+            };
 
-      addNewUser(newUser, user.uid);
+            addNewUser(newUser, user.uid);
 
-      navigate("/create-profile");
+            navigate("/create-profile");
+        }
     }
-  }
 
-  return user ? (
-    <Navigate to="/dependents" />
-  ) : (
-    <container
-      style={{
-        width: "100vw",
-        height: "100vh",
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "center",
-      }}
-    >
-      <div
-        style={{
-          display: "flex",
-          minWidth: "60vw",
-          minHeight: "100vh",
-          alignItems: "center",
-        }}
-      >
-        <Image
-          className={styles.heroImage}
-          src="https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg"
-          alt="Placeholder for Hero image"
-        />
-      </div>
-      <div className={styles.signInColumn}>
-        <Title className={styles.title}>Hey there!👋</Title>
-        <SignInButton />
-      </div>
-    </container>
-  );
+    return user ? (
+        <Navigate to="/dependents"/>
+    ) : (
+        <div
+            style={{
+                width: "100vw",
+                height: "100vh",
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+            }}
+        >
+            <div
+                style={{
+                    display: "flex",
+                    minWidth: "60vw",
+                    minHeight: "100vh",
+                    alignItems: "center",
+                }}
+            >
+                <Image
+                    className={styles.heroImage}
+                    src="https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg"
+                    alt="Placeholder for Hero image"
+                />
+            </div>
+            <div className={styles.signInColumn}>
+                <Title className={styles.title}>Hey there!👋</Title>
+                <SignInButton/>
+            </div>
+        </div>
+    );
 };
 
 export default Landing;
