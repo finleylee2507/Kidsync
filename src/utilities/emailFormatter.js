836a94aa68de94187b38b2d399c8bd877e38c5ff
Finley Li <li.f@wustl.edu>
2023-03-02 16:18:07 -0600

    It finally works

---
+++ src/utilities/emailFormatter.js
@@ -0,0 +1,23 @@
+
+
+export const fromEmailToDbString = async (email) => {
+    if (!email) {
+        throw new Error("Email cannot be empty");
+    }
+
+    const encoder = new TextEncoder();
+    const data = encoder.encode(email);
+    const hash = await crypto.subtle.digest('SHA-256', data);
+    const hex = Array.from(new Uint8Array(hash))
+        .map(b => b.toString(16).padStart(2, '0'))
+        .join('');
+    return hex;
+};
+
+export const fromDbStringToEmail = (dbString) => {
+  let originalEmail=dbString.replace(',','.')
+  return originalEmail
+};
+
+
+
