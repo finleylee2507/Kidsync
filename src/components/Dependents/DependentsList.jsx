ff1d090d8a622718bea426b065849634929feb5b
Finley Li <li.f@wustl.edu>
2023-03-04 21:07:53 -0600

    Added UI for delete dependent.

--- src/components/Dependents/DependentsList.jsx
+++ src/components/Dependents/DependentsList.jsx
@@ -1,7 +1,15 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import DependentCard from "./DependentCard";
 import styles from "./DependentsList.module.css";
-import { SimpleGrid, Container, createStyles, Button } from "@mantine/core";
+import {
+  SimpleGrid,
+  Container,
+  createStyles,
+  Button,
+  Modal,
+  Text,
+  Divider,
+} from "@mantine/core";
 import { useNavigate } from "react-router-dom";
 import { useDbData } from "../../utilities/firebase";
 import ExistingAccessModal from "../ExistingAccess/ExistingAccessModal";
@@ -32,9 +40,9 @@ const DependentsList = ({
   const [isOpenNewModal, setIsOpenNewModal] = useState(false);
   const [currentDependentName, setCurrentDependentName] = useState("");
   const [currentDependentID, setCurrentDependentID] = useState("");
+  const [isConfirmationModalOpen, setIsConfirmationModalOpen] = useState(false);
 
-
-    console.log("All deps: ",allDependents);
+  console.log("All deps: ", allDependents);
   useEffect(() => {
     if (allDependents) {
       setAllDeps(allDependents);
@@ -55,9 +63,15 @@ const DependentsList = ({
   // Pass the information of dependent
   const handleExistingModal = (state) => {
     setIsOpenExistingModal(state);
-
   };
 
+  const handleModalCancel = () => {
+    setIsConfirmationModalOpen(false);
+  };
+
+  const handleModalConfirm = () => {
+    console.log("Deleting dependent...");
+  };
   if (
     allDeps == null ||
     allDeps.length == 1 ||
@@ -90,11 +104,35 @@ const DependentsList = ({
   } else {
     return (
       <div>
+        <Modal
+          title={`You are about to delete ${currentDependentName}`}
+          opened={isConfirmationModalOpen}
+          onClose={() => setIsConfirmationModalOpen(false)}
+          classNames={{
+            title: styles.modalTitle,
+          }}
+          size="lg"
+        >
+          <Text>
+            Do you really want to delete this dependent? Doing so will delete
+            the dependent for his/her caretakers as well.
+          </Text>
+
+          <Divider my="sm" />
+          <div className={styles.modalButtonContainer}>
+            <Button variant="outline" onClick={handleModalCancel}>
+              Cancel
+            </Button>
+            <Button onClick={handleModalConfirm}>Confirm</Button>
+          </div>
+        </Modal>
         <ExistingAccessModal
           allUsers={allUsers}
           isOpen={isOpenExistingModal}
           handleModalState={setIsOpenExistingModal}
-          dependent={currentDependentID?allDependents[currentDependentID]:null}
+          dependent={
+            currentDependentID ? allDependents[currentDependentID] : null
+          }
         />
 
         <NewAccessModal
@@ -127,6 +165,7 @@ const DependentsList = ({
                   handleNewModalState={setIsOpenNewModal}
                   setCurrentDependentName={setCurrentDependentName}
                   setCurrentDependentID={setCurrentDependentID}
+                  setIsConfirmationModalOpen={setIsConfirmationModalOpen}
                 />
               )
             )}
