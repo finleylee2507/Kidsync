d7a1071ef21b4da30f03092d0f2198ee0c292ad9
Finley Li <li.f@wustl.edu>
2023-03-09 17:13:23 -0600

    Home page responsiveness fix

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from "react";
-import { SimpleGrid, Container } from "@mantine/core";
+import { SimpleGrid, Container, Grid } from "@mantine/core";
 import DependentCard from "./../Dependents/DependentCard";
 import ClientCard from "../Sitters/ClientCard";
 import clients from "../../utilities/clients.json";
 import styles from "./HomePage.module.css";
+import { useMediaQuery } from "@mantine/hooks";
 
 const HomePage = ({ user, allUsers, allDependents }) => {
   const [allDeps, setAllDeps] = useState(allDependents);
+  const isMobileSmall = useMediaQuery("(max-width:700px)");
+  const isMobileMedium = useMediaQuery("(max-width:1200px)");
 
   useEffect(() => {
     if (allDependents) {
@@ -19,27 +22,31 @@ const HomePage = ({ user, allUsers, allDependents }) => {
       <div className="wrapper">
         <div className={styles.sectionHeaderContainer}>
           <h1 className={styles.sectionHeader}>My Dependents</h1>
-          {/* <hr className={styles.headerBorder}></hr> */}
         </div>
-        <div className={styles.dependentsContainer}>
+        <Container size="xl" mx={0}>
           {!(
             allDeps == null ||
             allDeps.length == 1 ||
             allUsers[user.uid]["dependents"] == null
-          ) &&
-            Object.entries(allUsers[user.uid]["dependents"]).map(
-              ([id, dependentID]) => (
-                <DependentCard
-                  key={id}
-                  dependent={allDependents[dependentID]}
-                  showAll={false}
-                />
-              )
-            )}
-        </div>
+          ) && (
+            <Grid>
+              {Object.entries(allUsers[user.uid]["dependents"]).map(
+                ([id, dependentID]) => (
+                  <Grid.Col span={isMobileSmall ? 12 : isMobileMedium ? 6 : 4}>
+                    <DependentCard
+                      key={id}
+                      dependent={allDependents[dependentID]}
+                      showAll={false}
+                    />
+                  </Grid.Col>
+                )
+              )}
+            </Grid>
+          )}
+        </Container>
+
         <div className={styles.sectionHeaderContainer}>
           <h1 className={styles.sectionHeader}>In My Care</h1>
-          {/* <hr className={styles.headerBorder}></hr> */}
         </div>
         <div className={styles.dependentsContainer}>
           {!(
@@ -47,19 +54,27 @@ const HomePage = ({ user, allUsers, allDependents }) => {
             allDeps.length == 1 ||
             allUsers[user.uid] == undefined ||
             allUsers[user.uid].clients == null
-          ) &&
-            Object.entries(allUsers[user.uid].clients).map(([id, client]) => {
-              console.log(client);
-              return (
-                <ClientCard
-                  key={id}
-                  client={allDependents[client.id]}
-                  permissions={client.permissions}
-                  creator={allUsers[allDependents[client.id].creator]}
-                  currentUser={allUsers[user.uid]}
-                />
-              );
-            })}
+          ) && (
+            <Grid>
+              {Object.entries(allUsers[user.uid].clients).map(
+                ([id, client]) => {
+                  return (
+                    <Grid.Col
+                      span={isMobileSmall ? 12 : isMobileMedium ? 6 : 4}
+                    >
+                      <ClientCard
+                        key={id}
+                        client={allDependents[client.id]}
+                        permissions={client.permissions}
+                        creator={allUsers[allDependents[client.id].creator]}
+                        currentUser={allUsers[user.uid]}
+                      />
+                    </Grid.Col>
+                  );
+                }
+              )}
+            </Grid>
+          )}
         </div>
       </div>
     </React.Fragment>
