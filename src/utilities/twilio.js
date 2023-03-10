d3ecee595594617f84a13da3c0a91345ef7aa394
Che Hongle <chehongle123@163.com>
2023-03-09 22:38:10 -0600

    hotfix

--- src/utilities/twilio.js
+++ src/utilities/twilio.js
@@ -1,16 +1,15 @@
 const fromNumber = "+18339063844";
 const accountSid = "AC4335b291975545d0b99f0bc5ff28c250";
-const authToken = "03e6ea55eb65888b345407e1c52d85c5";
+const authToken = "9a3996afd3837e4c1f7910cad757b41d";
 
 export const sendSMS = (toNumber, message) => {
-  // verifyNumber(toNumber, "vedant");
-
   fetch(
-    "https://api.twilio.com/2010-04-01/Accounts/AC4335b291975545d0b99f0bc5ff28c250/Messages.json",
+    `https://api.twilio.com/2010-04-01/Accounts/${accountSid}/Messages.json`,
     {
       method: "POST",
       headers: {
-        Authorization: "Basic" + btoa(`${accountSid}:${authToken}`),
+        Authorization:
+          "Basic QUM0MzM1YjI5MTk3NTU0NWQwYjk5ZjBiYzVmZjI4YzI1MDo5YTM5OTZhZmQzODM3ZTRjMWY3OTEwY2FkNzU3YjQxZA==",
         "Content-Type": "application/x-www-form-urlencoded",
       },
       body: `From=${encodeURIComponent(fromNumber)}&To=${encodeURIComponent(
@@ -19,9 +18,11 @@ export const sendSMS = (toNumber, message) => {
     }
   )
     .then((response) => {
+      console.log(toNumber + message);
       console.log(response.status);
     })
     .catch((error) => {
+      console.log(toNumber + message);
       console.error(error);
     });
 };
